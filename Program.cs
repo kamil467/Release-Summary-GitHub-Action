d108a15cec93cfdb3292b4e6f5c4ef8683f37930
Kamil Hussain <31802480+kamil467@users.noreply.github.com>
2023-07-07 20:23:39 +0000

    updated release summary markdown

--- Program.cs
+++ Program.cs
@@ -16,10 +16,53 @@ parser.WithNotParsed(
                 string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
 
         Environment.Exit(2);
-    }).WithParsed( inputs =>{
-        Console.WriteLine($" Owner is : Name is {inputs.Owner}");
+    }).WithParsed( async inputs =>{
+       
+       await PrintReleaseSummaryAsync(inputs,host);
 
-        Environment.Exit(0);
     });
+
+static async ValueTask PrintReleaseSummaryAsync(ActionInputs inputs, IHost host)
+{
+  
+ //    
+        
+        string line = null;
+        try
+      {
+    //Pass the file path and file name to the StreamReader constructor
+    StreamReader sr = new StreamReader("release.md");
+    //Read the first line of text
+    line = sr.ReadLine();
+    //Continue to read until you reach end of file
+    while (line != null)
+    {
+        //Read the next line
+        Console.WriteLine(line);
+        line = sr.ReadLine();
+    }
+    //close the file
+    sr.Close();
+
+    var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+    if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
+    {
+        using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
+        textWriter.WriteLine($"summary-details={line}");
+    }
+
+}
+catch(Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+
+        await ValueTask.CompletedTask;
+        Console.WriteLine($" Owner is : Name is {inputs.Owner}");
+        Environment.Exit(0);
+
+}
+
+
 
 await host.RunAsync();
